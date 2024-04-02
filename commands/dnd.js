4fc7912d91a8a247101181ffb6a1eb8834812781
TJPerson04 <HiPerson929@gmail.com>
2024-04-02 13:47:54 -0400

    Added testing stuff

--- commands/dnd.js
+++ commands/dnd.js
@@ -2,7 +2,14 @@
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, getVoiceConnection } = require('@discordjs/voice');
 const { SlashCommandBuilder } = require("discord.js");
 const { join } = require('node:path');
-const { client } = require('../index')
+
+const { getYTTitle } = require("..//helpers/helper_functions");
+
+// IDEAS
+// Differentiate song title from artist (different font? bold? italics?)
+// Length of song
+// Number inside the queue (queueIndex + 1)
+// Selection drop down bar to choose a specific song to play?
 
 require('dotenv').config();
 
@@ -17,99 +24,69 @@ module.exports = {
                 .setDescription('The song to play')
                 .setRequired(true)),
     execute: async (interaction) => {
-        const lib = require('lib')({ token: process.env.STDLIB_SECRET_TOKEN });
-        let test3 = await interaction.guild.emojis.fetch()
-            .then(emojis => {
-                return emojis.find(x => x.name == 'Namess' || x.toString() == 'Namess')
-            }).catch(err => {console.error(err)})
-        let test2 = await interaction.guild.emojis
-        // await interaction.guild.emojis.create({attachment: 'https://npr.brightspotcdn.com/dims4/default/67c6173/2147483647/strip/true/crop/1023x912+0+0/resize/880x785!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2F2b%2F18%2Fc042b647406185b7de55c332432e%2Fmeeko-racoon-01-1024x1024.jpg', name: 'Namess'})
-        // await interaction.guild.emojis.edit({name: 'Namess', attachment: 'https://people.com/thmb/CVCMobv9fzMPSdgy8h1eCOfZKPY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x0:1001x2)/raccoon1-271156448c6e4008aea8535e1eec4ca5.jpg'})
-        // await test3.edit({attachment: 'https://people.com/thmb/CVCMobv9fzMPSdgy8h1eCOfZKPY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x0:1001x2)/raccoon1-271156448c6e4008aea8535e1eec4ca5.jpg'})
-        await test3.delete()
-        let test = {
+        let outputMessage = {
+            "url": "https://www.youtube.com/watch?v=Hv72-q5m0uI",
             "content": "",
             "tts": false,
             "embeds": [
                 {
                     "type": "rich",
-                    "title": `Queue`,
-                    "description": "",
-                    "color": 0xffe897,
-                    "fields": [
-                        {
-                            "name": `Currently Playing`,
-                            "value": `Feather - Sabrina Carpenter`,
-                        },
-                        {
-                            "name": `Up Next`,
-                            "value": `<:pause:1124381774844285008>exes - Tate McRae\nSunflower - Spidermand\nTraining Season - Dua Lipa`
-                        }
-                    ]
+                    "title": `So Right`,
+                    "description": `*[Carly Rae Jepsen](https://www.youtube.com/@carlyraemusic)*`,
+                    "color": `${0xffe897}`,
+                    "image": {
+                        "url": `https://cdn.discordapp.com/attachments/887793959089340466/1222687748872343602/so-right-song.png?ex=66171fb3&is=6604aab3&hm=5bcf9b321854da7c906b1c5ec0d4eb4a62e47d208275e83ffa086d0e35e93844&`,
+                        "height": 0,
+                        "width": 0
+                    },
+                    "author": {
+                        "name": `Currently Playing `
+                    },
+                    "footer": {
+                        "text": `Added by Me :)`,  // If the user has a nickname in this server, use that. Otherwise, just use their username
+                        "icon_url": `https://cdn.discordapp.com/attachments/887793959089340466/1222219370801926234/cake-song.png?ex=66156b7d&is=6602f67d&hm=3befa2bc34276859762be778dac8784337286aeb302eb7149e4f0530e878617b&`  // If the user has a server specific avatar, display that. Otherwise, just use their global avatar
+                    }
                 }
             ]
         }
 
-            interaction.reply(test);
-        // const song = interaction.options.getString('song');
-        // const textChannel = interaction.channel;
-        // let filePath;
+        let outputMessage2 = {
+            "url": "https://www.youtube.com/watch?v=Hv72-q5m0uI",
+            "content": "",
+            "tts": false,
+            "embeds": [
+                {
+                    "type": "rich",
+                    // "description": `*[So Right](https://www.youtube.com/watch?v=MaLryQ80leE)* - *[Carly Rae Jepsen](https://www.youtube.com/channel/UC-4NAdFIP6h9BmtGUFL94Pg)*`,
+                    // "description": ``,
+                    "color": `${0xffe897}`,
+                    // "thumbnail": {
+                    //     "url": `https://cdn.discordapp.com/attachments/887793959089340466/1222220884492816607/so-right-song.png?ex=66156ce6&is=6602f7e6&hm=3f6bdb0a601b7fc5986561c0605882290e9eac7e87530ea638d427919c618262&`,
+                    //     "height": 100,
+                    //     "width": 100
+                    // },
+                    // "footer": {
+                    //     "text": `Currently Playing So Right - Carly Rae Jepsen`,
+                    //     "url": "https://www.youtube.com/watch?v=9J_qJYi7IBg",
+                    //     "icon_url": `https://cdn.discordapp.com/attachments/887793959089340466/1222220884492816607/so-right-song.png?ex=66156ce6&is=6602f7e6&hm=3f6bdb0a601b7fc5986561c0605882290e9eac7e87530ea638d427919c618262&`
+                    // },
+                    // "fields": [
+                    //     {
+                    //         "name": `Up Next`,
+                    //         "value": "\u200B",
+                    //         "inline": false
+                    //     }
+                    // ],
+                    "image": {
+                        "url": `https://media.discordapp.net/attachments/887793959089340466/1222231996277981195/output-image.png?ex=6615773f&is=6603023f&hm=0f831d35977b37c6c5f1842f4b28bb2cb405e5dbcef24f285125296616232185&=&format=webp&quality=lossless&width=1100&height=618`,
+                        "height": 0,
+                        "width": 0
+                    },
+                    "title": `Up Next`
+                }
+            ]
+        }
 
-        // let voiceChannelId;
-        // if (interaction.member.voice.channel) {
-        //     voiceChannelId = interaction.member.voice.channel.id;
-        // } else {
-        //     await interaction.reply('You must be in a voice channel to use this command');
-        //     return
-        // }
-
-        // switch (song) {
-        //     case 'panic':
-        //         filePath = join(__dirname, '\\..\\dnd\\LobotomyCorporation_OST_Second_Warning.mp3');
-        //         break;
-        //     case 'church':
-        //         filePath = join(__dirname, '\\..\\dnd\\Arknights_OST_bat_white_trans.mp3');
-        //         break;
-        //     case '2':
-        //         filePath = join(__dirname, '\\..\\dnd\\Arknights_OST_bat_act17side_01.mp3')
-        //         break;
-        // }
-
-        // if (!filePath) {
-        //     interaction.reply('Unkown song');
-        //     return
-        // }
-
-        // let connection;
-        // if (getVoiceConnection(interaction.guildId)) {
-        //     connection = getVoiceConnection(interaction.guildId);
-        // } else {
-        //     connection = joinVoiceChannel({
-        //         channelId: voiceChannelId,
-        //         guildId: textChannel.guild.id,
-        //         adapterCreator: textChannel.guild.voiceAdapterCreator,
-        //     })
-        // }
-        // const player = createAudioPlayer();
-        // const resource = createAudioResource(filePath);
-
-        // connection.subscribe(player);
-        // player.play(resource);
-
-        // //I have absolutely no clue what this bit of code does, stole it off the internet and it fixed a bug
-        // const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
-        //     const newUdp = Reflect.get(newNetworkState, 'udp');
-        //     clearInterval(newUdp?.keepAliveInterval);
-        // }
-
-        // connection.on('stateChange', (oldState, newState) => {
-        //     const oldNetworking = Reflect.get(oldState, 'networking');
-        //     const newNetworking = Reflect.get(newState, 'networking');
-
-        //     oldNetworking?.off('stateChange', networkStateChangeHandler);
-        //     newNetworking?.on('stateChange', networkStateChangeHandler);
-        // });
-
-        // interaction.reply('Playing song');
+        interaction.reply(outputMessage2);
     }
 }
